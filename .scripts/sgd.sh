032f51d6977fa9bc6885e90377fdad6aced6961c
teral <teral@T460.TERALium>
2019-05-09 15:30:22 +0100

    xx

--- .scripts/sgd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# script copies markdown files to a GDrive shared location.
-
-cp -v $HOME/docs/commands.md $HOME/docs/gdrive-share/
-cp -v $HOME/docs/cisco-commands.md $HOME/docs/gdrive-share/
-cp -v $HOME/docs/journal.md $HOME/docs/gdrive-share/
